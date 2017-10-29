e6cdbe50973735515bbc7b911e43e03c06de760f
Rustem Kakimov <ruskakimov@gmail.com>
2017-10-29 09:53:13 +0800

    use component folder pattern for splitpage

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import reducers from './reducers'
+import reducers from '@reducers'
 import App from './App'
 
 ReactDOM.render(
