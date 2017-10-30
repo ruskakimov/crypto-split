11511869d509b5f5c8d5e5a7bd2e7d56d2d9c004
Rustem Kakimov <ruskakimov@gmail.com>
2017-10-30 13:26:47 +0800

    implement combine page reducer

--- src/@constants/index.js
+++ src/@constants/index.js
@@ -13,6 +13,9 @@ export const fonts = {
 export const SUBMIT_MESSAGE = 'SUBMIT_MESSAGE'
 export const CHANGE_NUMBER_OF_SPLIT_KEYS = 'CHANGE_NUMBER_OF_SPLIT_KEYS'
 
+export const CHANGE_KEY_FIELD_VALUE = 'CHANGE_KEY_FIELD_VALUE'
+export const SUBMIT_KEY = 'SUBMIT_KEY'
+
 // app
 export const MIN_KEYS = 2
 export const MAX_KEYS = 10
--- src/@reducers/combinePage.js
+++ src/@reducers/combinePage.js
@@ -1,8 +1,50 @@
+import { CHANGE_KEY_FIELD_VALUE, SUBMIT_KEY } from '@constants'
+import { produceMessage } from '@helpers'
+
+function handleSubmitKey(state, action) {
+  if (state.keys.length === 0 ||
+      state.keys[0].length === action.payload.length)
+  {
+    const newKeys = [ action.payload, ...state.keys ]
+    return {
+      message: produceMessage(newKeys),
+      keyField: {
+        value: '',
+        errorMessage: null
+      },
+      keys: newKeys
+    }
+  }
+  return {
+    ...state,
+    keyField: {
+      ...state.keyField,
+      errorMessage: `Key does not match the length of previously entered keys!`
+    }
+  }
+}
+
+
 const combinePage = (state = {
   message: '',
+  keyField: {
+    value: '',
+    errorMessage: null
+  },
   keys: []
-}, action) => {
+}
+, action) => {
   switch (action.type) {
+    case CHANGE_KEY_FIELD_VALUE:
+      return {
+        ...state,
+        keyField: {
+          value: action.payload,
+          errorMessage: null
+        }
+      }
+    case SUBMIT_KEY:
+      return handleSubmitKey(state, action)
     default:
       return state
   }
