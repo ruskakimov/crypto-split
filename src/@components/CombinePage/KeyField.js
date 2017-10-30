6d41718d6b3da571fd1fb712a2857b90d7e9d0cc
Rustem Kakimov <ruskakimov@gmail.com>
2017-10-30 13:35:16 +0800

    setup key field component boilerplate

---
+++ src/@components/CombinePage/KeyField.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class KeyField extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text"/>
+      </form>
+    )
+  }
+}
+
+export default KeyField
--- src/@components/CombinePage/index.js
+++ src/@components/CombinePage/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Message from './Message'
 import KeysList from './KeysList'
+import KeyField from './KeyField'
 
 const CombinePage = (props) => {
   return (
     <div>
       <h1>Combine page</h1>
+      <KeyField/>
       <Message/>
       <KeysList/>
     </div>
