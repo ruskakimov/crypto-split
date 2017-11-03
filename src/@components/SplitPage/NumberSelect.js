f58ac2303395fbd77584291e549dc47774c3ee1a
Rustem Kakimov <ruskakimov@gmail.com>
2017-11-03 16:21:16 +0800

    style select

--- src/@components/SplitPage/NumberSelect.js
+++ src/@components/SplitPage/NumberSelect.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { changeNumberOfSplitKeys } from '@actions'
 import { MIN_KEYS, MAX_KEYS } from '@constants'
+import styled from 'styled-components'
+import { colors } from '@constants'
+
+const Select = styled.select`
+  background: ${colors.black};
+  color: ${colors.neon_green};
+  border: 2px solid ${colors.neon_green};
+  margin-left: 10px;
+`
 
 class NumberSelect extends Component {
   handleChange = (e) => {
@@ -20,9 +29,12 @@ class NumberSelect extends Component {
 
   render() {
     return (
-      <select value={this.props.number} onChange={this.handleChange}>
-        {this.generateOptions()}
-      </select>
+      <div>
+        Number of keys:
+        <Select value={this.props.number} onChange={this.handleChange}>
+          {this.generateOptions()}
+        </Select>
+      </div>
     )
   }
 }
